49befe6303883d6dab3a85bc8e599aa5f97e915a
SamuelUnal <scardenaslo@unal.edu.co>
2026-03-19 08:54:34 -0500

    versionPreuba

--- PPPOO/src/Tipo2/ContenidoMultimedia.java
+++ PPPOO/src/Tipo2/ContenidoMultimedia.java
@@ -2,19 +2,19 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package pppoo;
+package Tipo2;
 
 /**
  *
  * @author Usuario
  */
-public class PPPOO {
+public class ContenidoMultimedia {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Cacorro");
     }
     
-}
+}  
